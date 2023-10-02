d3ac7bfb41558e1b18f3f8d38c778c08740d2f77
Abraham Geller <AGeller@bayviewglen.ca>
2023-10-02 14:51:41 -0400

    example 2,3, and 4

---
+++ Unit One - Foundations of Python Programming/Day 6- Formatting f-strings/example3.py
@@ -0,0 +1,13 @@
+
+import math
+
+average = 67.88878
+
+print(f"{average}")
+print(f"{average:.2f}")
+print(f"{average:>20}")
+print(f"{average:>.2f}")
+
+
+print(math.pi)
+print(f"{math.pi:>10.2f}")

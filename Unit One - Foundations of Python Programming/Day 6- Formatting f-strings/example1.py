6fc536079e8690d62ba5f1862bff46469619efc0
Abraham Geller <AGeller@bayviewglen.ca>
2023-10-02 14:19:03 -0400

    example one of f formations

---
+++ Unit One - Foundations of Python Programming/Day 6- Formatting f-strings/example1.py
@@ -0,0 +1,19 @@
+average = 12.65789
+print(f"{average}") #12.65789
+print(f"{average:.2f}") # rounds to 2 decimal places 12.66
+print(f"{average:.3f}") #12.658
+
+number = 6.1 #6.10
+print(f"{number:.2f}")
+
+number = 6
+print(f"{number:.2f}") #6.00
+
+math_mark = 34
+math_total = 45
+
+math_average= math_mark/math_total
+
+print(f"{math_average}")  #0.75555555
+print(f"{math_average:.1%}") #75.6 %
+print(f"{math_average:.1f}") #0.8

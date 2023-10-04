d65bdf14d348af78ed193f5e75fc15eb05d0bc7c
Abraham Geller <AGeller@bayviewglen.ca>
2023-10-04 13:54:11 -0400

    Dots and boxes unfinished

---
+++ Unit One - Foundations of Python Programming/Day 8- Dots and boxes/Dots.py
@@ -0,0 +1,40 @@
+name = str(input("Enter your name:"))
+
+print("Game #1:")
+print("Opponents name: Alex lee")
+print("Your points: 17")
+print("Opponents points: 19")
+
+
+
+print("Game #2:")
+print("Opponents name:Ishaan")
+print("Your points: 4")
+print("Opponents points:32")
+
+
+
+print("Game #3:")
+print("Opponents name: Arees")
+print("Your points: 34")
+print("Opponents points: 2")
+
+
+
+print("Game #4:")
+print("Opponents name: Brandon")
+print("Your points: 13")
+print("Opponents points: 23")
+
+
+
+print("Game #5:")
+print("Opponents name: Keaton")
+print("Your points: 10")
+print("Opponents points: 26")
+
+print("Dots and boxes Score Tracker")
+
+print("Player name: Abraham")
+
+Opponent_game1 = "Alex lee"

3fded57ac3ff4dacb278d7a4e48a734cc9a3e7b7
Abraham Geller <AGeller@bayviewglen.ca>
2023-10-04 20:06:20 -0400

    finished dots and boxes

--- Unit One - Foundations of Python Programming/Day 8- Dots and boxes/Dots.py
+++ Unit One - Foundations of Python Programming/Day 8- Dots and boxes/Dots.py
@@ -38,3 +38,40 @@ print("Dots and boxes Score Tracker")
 print("Player name: Abraham")
 
 Opponent_game1 = "Alex lee"
+your_points1 = "17"
+opponent_points1 = "19"
+box_percentage1 = "47.2%"
+
+Opponent_game2 = "Ishaan"
+your_points2 = "4"
+opponent_points2 = "32"
+box_percentage2 = "11.1%"
+
+Opponent_game3 = "Arees"
+your_points3 ="34"
+opponent_points3 = "2"
+box_percentage3 = "94.4%"
+
+Opponent_game4 = "Brandon"
+your_points4 ="13"
+opponent_points4 = "23"
+box_percentage4 = "36.1%"
+
+Opponent_game5 = "Keaton"
+your_points5 ="10"
+opponent_points5 = "26"
+box_percentage5 = "27.8%"
+print("----------------------------------------------------------------------------")
+print(f"{'Opponent':<10} {'Your points':>10} {'Opponent Points' :>10} {'Box Percentage':<20}")
+print(f"{Opponent_game1:<10} {your_points1:>10} {opponent_points1:>10} {box_percentage1:>20}")
+print(f"{Opponent_game2:<10} {your_points2:>10} {opponent_points2:>10} {box_percentage2:>20}")
+print(f"{Opponent_game3:<10} {your_points3:>10} {opponent_points3:>10} {box_percentage3:>20}")
+print(f"{Opponent_game4:<10} {your_points4:>10} {opponent_points4:>10} {box_percentage4:>20}")
+print(f"{Opponent_game5:<18} {your_points5:<10} {opponent_points5:<10} {box_percentage5:^20}")
+print("----------------------------------------------------------------------------")
+
+
+print("Summary:")
+print("Total points: 78")
+print("Total opponents points: 102")
+print("Percentage points Recieved: 43.32%")

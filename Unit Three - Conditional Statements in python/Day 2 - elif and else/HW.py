692574b7ff59369d3a5f22c868a1fe8694124c6b
Abraham Geller <AGeller@bayviewglen.ca>
2023-11-08 08:37:54 -0500

    Day 2 lesson and some HW

---
+++ Unit Three - Conditional Statements in python/Day 2 - elif and else/HW.py
@@ -0,0 +1,20 @@
+def in1020(a,b):
+    if 10 <= a <= 20:
+        return True
+    elif 10<= b <= 20:
+        return True
+    else:
+        return False
+
+def squirrel_play(temp, is_summer):
+    if is_summer:
+        if 60 <= temp <= 100:
+            return True
+        else:
+            return False
+    else:
+        if 60 <= temp <= 100:
+            return True
+        else:
+            return False
+        

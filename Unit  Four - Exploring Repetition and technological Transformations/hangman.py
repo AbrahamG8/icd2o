d8cb76f683203ba48f631ab8c6783d6a0ac9fa37
Abraham Geller <AGeller@bayviewglen.ca>
2023-11-29 09:34:45 -0500

    hangman

--- Unit  Four - Exploring Repetition and technological Transformations/hangman.py
+++ Unit  Four - Exploring Repetition and technological Transformations/hangman.py
@@ -1,52 +1,57 @@
 import random
-
+ 
 def choose_word():
-    words = ["weed", "monkey", "football", "basketball", "KFC", "Popeyes", "alexidol"]
+    words = ["monkey", "football", "basketball", "KFC", "Popeyes", "Fried chicken", "Watermelon", "black air forces", "black nike tech", "rap music", "stealing", "Nayvadius", "Shéyaa"]
     return random.choice(words)
-
-def display_word(word, guessed_word)
-    display = ''
+ 
+def display_word(word, guessed_letters):
+    display = ""
     for letter in word:
         if letter in guessed_letters:
             display += letter
         else:
-            display += '_'
+            display += "_"
     return display
-
+ 
+def is_lowercase_letter(char):
+    return 'a' <= char <= 'z'
+ 
 def hangman():
-    words = ['python', 'hangman', 'programming', 'challenge', 'coding']
-    word_to_guess = random.choice(words).lower()
-    guessed_letters = set()
-    attempts = 6
-
     print("Welcome to Hangman!")
-    print(display_word(word_to_guess, guessed_letters))
-
-    while '_' in display_word(word_to_guess, guessed_letters) and attempts > 0:
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = 10
+ 
+    while attempts > 0:
+        current_display = display_word(word_to_guess, guessed_letters)
+        print(f"Word: {current_display}")
+        print(f"Guessed letters: {', '.join(guessed_letters)}")
+        print(f"Attempts left: {attempts}")
+ 
         guess = input("Guess a letter: ").lower()
-
-        if len(guess) != 1 or not guess.isalpha():
-            print("Invalid input. Please enter a single letter.")
-            continue
-
-        if guess in guessed_letters:
-            print("You've already guessed that letter. Try again.")
-            continue
-
-        guessed_letters.add(guess)
-
-        if guess not in word_to_guess:
-            attempts -= 1
-            print(f"Incorrect! Attempts left: {attempts}")
+ 
+        if len(guess) != 1 or not is_lowercase_letter(guess):
+            print("Please enter a valid single letter.")
+           
+ 
+        if guess not in guessed_letters:
+            guessed_letters.append(guess)
         else:
-            print("Correct!")
-
-        print(display_word(word_to_guess, guessed_letters))
-
-    if '_' not in display_word(word_to_guess, guessed_letters):
-        print("Congratulations! You guessed the word.")
-    else:
-        print(f"Sorry, you ran out of attempts. The word was '{word_to_guess}'.")
-
+            print("You've already guessed that letter. Try again.")
+           
+ 
+        if guess not in word_to_guess:
+            print("Incorrect guess!")
+            attempts -= 1
+        else:
+            print("Correct guess!")
+ 
+        if set(guessed_letters) == set(letter for letter in word_to_guess):
+            print("Congratulations! You've guessed the word:", word_to_guess)
+            break
+ 
+        if attempts == 0:
+            print("Sorry, you've run out of attempts. The word was:", word_to_guess)
+ 
 if __name__ == "__main__":
     hangman()

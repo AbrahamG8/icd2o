556b371b3dfa2952a65a1bb8ab90598c7217d8ff
Abraham Geller <AGeller@bayviewglen.ca>
2024-01-17 11:00:40 -0500

    lesson 1, 2, 3

---
+++ Unit Five - Exploring lists and Addressig Digital Technology/Day 2 - Iterate a list/basic- iteration.py
@@ -0,0 +1,54 @@
+# list = [4,5,3,5,3,45,6,4,3,4,3,4,2,3,3,-6,-6, -7, -69]
+# # print(list)
+
+# # for element in list:
+# #     # print(element)
+# #  list = {1,2,3,4, 5}
+# #  total = 0
+# # for el in list:
+# #     total += el
+
+# # print(el)
+
+
+# # print(sum(list))
+
+# # list = [1,2,3,4,5,6,7,8,90]
+
+# # for el in list:
+# #    if el % 2 == 0 and el > 0:
+# #       print(el)
+    
+# # smallest = list[0]
+# # for alex in list:
+# #    if alex < smallest:
+# #         smallest = alex
+
+# # print(smallest)
+
+# # print (min(list))
+# # print (max(list))
+
+# word_list = ["happy","Ishaan", "monkizzy", "godizzy", "Gizzy G"]
+# biggest_name = word_list[0]
+# for alex in word_list:
+#     if alex > biggest_name:
+#         biggest_name = alex
+
+# print(max(word_list))
+# print(max(word_list))
+
+# check = ["A", "a"]
+# print(max(check))
+
+# longest = word_list[0]
+
+# for alex in word_list:
+#     if len(longest) < len(alex) :
+#         longest = alex
+
+# print(longest)
+
+# print(max(word_list, key=len))
+# print(max(word_list))
+
